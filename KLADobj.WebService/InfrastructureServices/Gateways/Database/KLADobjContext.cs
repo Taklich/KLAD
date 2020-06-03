0e9229993fcd201d6389557dfe4c8abe0a8781fd
Taklich <66114641+Taklich@users.noreply.github.com>
2020-06-03 14:14:03 +0300

    Lab7

--- KLADobj.WebService/InfrastructureServices/Gateways/Database/KLADobjContext.cs
+++ KLADobj.WebService/InfrastructureServices/Gateways/Database/KLADobjContext.cs
@@ -3,6 +3,7 @@ using KLADobj.DomainObjects;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using KLADobj.ApplicationServices.Synchronization;
 
 namespace KLADobj.InfrastructureServices.Gateways.Database
 {
@@ -14,53 +15,11 @@ namespace KLADobj.InfrastructureServices.Gateways.Database
             : base(options)
         { }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            FillTestData(modelBuilder);
-        }
-        private void FillTestData(ModelBuilder modelBuilder)
-        {
+            var v = new UseCaseKLADobj();
 
-            modelBuilder.Entity<kladobj>().HasData(
-                new
-                {
-                    Id = 1L,
-                    Name = "Домодедовское кладбище",
-                    Address = "Московская область, г. Домодедово, вблизи д. Истомиха, территория Домодедовское кладбище",
-                    ChillType = "урной в землю, гробом в землю",
-                    Free = "закрытое",
-                    
-                },
-                new
-                {
-                    Id = 2L,
-                    Name = "Жохово кладбище",
-                    Address = "деревня Жохово",
-                    ChillType = "гробом в землю",
-                    Free = "закрытое",
-
-                },
-                new
-                {
-                    Id = 3L,
-                    Name = "Захарьинское кладбище",
-                    Address = "улица Николая Сироткина",
-                    ChillType = "урной в землю,гробом в землю",
-                    Free = "открытое",
-
-                },
-                new
-                {
-                    Id = 4L,
-                    Name = "Зеленоградское (Северное) кладбище",
-                    Address = "Зеленоград, Проезд N 710, дом 19 стр. 1, 3, 4",
-                    ChillType = "урной в открытый колумбарий, урной в землю, гробом в землю",
-                    Free = "закрытое",
-
-                }
-               
-            );
+            modelBuilder.Entity<kladobj>().HasData(v.kladobjs);
         }
     }
 }
